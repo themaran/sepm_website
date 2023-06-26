2c7028c04ccfa451c37232bd04c8fd956ddb9426
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2023-06-26 21:24:33 +0530

    Add menu

--- sepm_web/src/components/Menu.js
+++ sepm_web/src/components/Menu.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../assets/css/menu.css'
 
 export const Menu = () => {
+    const [isAdmin, setAdmin] = useState(false);
   return (
-    <div>Menu</div>
+    <div className='menu'>
+            <Link to='/'>Home</Link>
+            <Link to='/classes'>Classes</Link>
+            { isAdmin ? <Link to='/user'>Add User</Link> : <></>}
+
+            
+            <div className='divider d-none d-md-block'></div>
+            <div className='footer d-none d-md-block'>
+                <p>&#169; MAMCET 2023</p>
+                <p>All rights served</p>
+            </div>
+    </div>
   )
 }
--- sepm_web/src/pages/Home.js
+++ sepm_web/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Menu } from '../components/Menu'
 
 export const Home = () => {
   return (
     <>
         <Header/>
+        <Menu/>
     </>
   )
 }
