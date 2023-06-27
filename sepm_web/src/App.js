4a24e10b79a0be91e428fa3f6c54e7ca9be44d59
_mad_maran_ <elamaran.infotech@gmail.com>
2023-06-27 05:09:05 +0100

    error by me

--- sepm_web/src/App.js
+++ sepm_web/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Home } from './pages/Home';
+import { Add }  from './pages/Add';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path="/class/Add.js" element={ <Add/> }/>
         </Routes>
       </Router>
     </>
---
+++ sepm_web/src/pages/Add.js
@@ -0,0 +1,42 @@
+import './Add.css';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+        <Form/>
+    </>
+  );
+}
+
+function Form(){
+    return(
+        <>
+        <h2>Add Students</h2>
+        <form>
+            <label>Reg. No</label>
+            <input type='phone'></input>
+            <br></br>
+            <label>Reg. No</label>
+            <input type='phone'></input>
+            <br></br>
+            <label>Reg. No</label>
+            <input type='phone'></input>
+            <br></br>
+            <label>Reg. No</label>
+            <input type='phone'></input>
+            <br></br>
+            <label>Reg. No</label>
+            <input type='phone'></input>
+            <br></br>
+            <label>Reg. No</label>
+            <input type='phone'></input>
+            <br></br>
+            <button></button>
+            <br></br>
+            <button></button>
+        </form>
+        </>
+    )
+}
+export default App;
