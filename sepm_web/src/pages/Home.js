0e19786aa12915d656392171bdca21a130efc6e0
mkandan1 <64264387+mkandan1@users.noreply.github.com>
2023-07-12 11:39:59 +0530

    Update Home.js

--- sepm_web/src/pages/Home.js
+++ sepm_web/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from '../components/Header'
 import { Menu } from '../components/Menu'
 import '../assets/css/home.css'
@@ -43,26 +43,124 @@ const romanNumbers = {
 }
 
 export const Home = () => {
+
+  const [unread, setUnread] = useState([]);
   return (
     <>
 
       <div className='bg-gray-100 w-full h-screen relative pt-28 pl-5 pr-5 sm:pl-64'>
-        <div className='grid auto-cols-auto sm:grid-cols-3'>
-          <h1 className='font-extrabold md:text-2xl sm:text-xl text-lg tracking-tight text-gray-800 col-auto sm:col-span-2'>Dashboard</h1>
+        {/* Page Heading */}
+        <h1 className='font-extrabold md:text-2xl sm:text-xl text-lg tracking-tight text-gray-800 mb-5'>Dashboard</h1>
+        {/* Grid system */}
+        <div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-3 xl:grid-cols-6 grid-rows-6 gap-3'>
 
-          <div className='bg-white grid col-span-3 border pl-10 pt-5 pb-5 pr-10 rounded-md relative'>
-            <div className='flex col-span-0 flex-col justify-center items-center bg-green-200 rounded-full w-7 md:w-8 h-7 md:h-8'>
+          {/* Grid 1 */}
+          <div className='bg-white grid grid-cols-3 col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-1 border p-5 sm:pl-5 sm:pt-5 sm:pb-5 sm:pr-5 rounded-md'>
+            <div className='flex flex-col justify-center justify-self-center items-center bg-green-200 rounded-full w-7 md:w-8 h-7 md:h-8'>
               <span class="material-symbols-outlined text-sm">
                 group
               </span>
             </div>
-            <div className=''>
-              <h2 className='text-gray-400 text-sm font-medium'>I YEAR STUDENTS</h2>
+            <div className='col-span-2'>
+              <h2 className='text-gray-400 text-xs font-medium'>I YEAR STUDENTS</h2>
+              <p className='text-xl font-semibold mt-2'>62</p>
+              <Link className='text-sm text-blue-500 mt-2'>Manage</Link>
+            </div>
+          </div>
+
+          {/* Grid 2 */}
+          <div className='bg-white grid grid-cols-3 col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-1 border p-5 sm:pl-5 sm:pt-5 sm:pb-5 sm:pr-5 rounded-md'>
+            <div className='flex flex-col justify-center justify-self-center items-center bg-green-200 rounded-full w-7 md:w-8 h-7 md:h-8'>
+              <span class="material-symbols-outlined text-sm">
+                group
+              </span>
+            </div>
+            <div className='col-span-2'>
+              <h2 className='text-gray-400 text-xs font-medium'>II YEAR STUDENTS</h2>
               <p className='text-xl font-semibold mt-2'>58</p>
               <Link className='text-sm text-blue-500 mt-2'>Manage</Link>
             </div>
           </div>
+
+          {/* Grid 3 */}
+          <div className='bg-white grid grid-cols-3 col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-1 border p-5 sm:pl-5 sm:pt-5 sm:pb-5 sm:pr-5 rounded-md'>
+            <div className='flex flex-col justify-center justify-self-center items-center bg-green-200 rounded-full w-7 md:w-8 h-7 md:h-8'>
+              <span class="material-symbols-outlined text-sm">
+                group
+              </span>
+            </div>
+            <div className='col-span-2'>
+              <h2 className='text-gray-400 text-xs font-medium'>III YEAR STUDENTS</h2>
+              <p className='text-xl font-semibold mt-2'>55</p>
+              <Link className='text-sm text-blue-500 mt-2'>Manage</Link>
+            </div>
+          </div>
+
+          {/* Grid 4 */}
+          <div className='bg-white grid grid-cols-3 col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-1 border p-5 sm:pl-5 sm:pt-5 sm:pb-5 sm:pr-5 rounded-md'>
+            <div className='flex flex-col justify-center justify-self-center items-center bg-green-200 rounded-full w-7 md:w-8 h-7 md:h-8'>
+              <span class="material-symbols-outlined text-sm">
+                group
+              </span>
+            </div>
+            <div className='col-span-2'>
+              <h2 className='text-gray-400 text-xs font-medium'>IV YEAR STUDENTS</h2>
+              <p className='text-xl font-semibold mt-2'>50</p>
+              <Link className='text-sm text-blue-500 mt-2'>Manage</Link>
+            </div>
+          </div>
+          {/* Grid 5 */}
+          <div className='col-span-2 row-span-2 sm:col-span-4 lg:col-span-2 sm:row-span-2 lg:row-span-3 border rounded-md bg-white pt-8 pl-8'>
+            <h1 className='font-bold text-xl tracking-tighter text-gray-700'>Notification</h1>
+            <div className='grid grid-cols-2 '>
+              {
+                unread.length >= 1 ?
+                  <>
+                    <div className='col-span-2 mt-2 flex'>
+                      <ul>
+                        {unread.map((mesaage) => {
+                          return (
+                            <li key={mesaage.id} className='text-sm mt-3'><b>{mesaage.sender}<span className='text-xs text-gray-500'>({mesaage.sender_role})</span></b> - {mesaage.message}</li>
+                          )
+                        })}
+                      </ul>
+                    </div>
+                  </> :
+                  <>
+                    <div className='col-span-2 mt-20 flex justify-center items-center'>
+                      <p>Upcoming feature</p>
+                    </div>
+                  </>
+              }
+            </div>
+          </div>
+          {/* Grid 6 */}
+          <div className='col-span-2 row-span-2 sm:col-span-4 lg:col-span-3 sm:row-span-2 lg:row-span-3 border rounded-md bg-white pt-8 pl-8'>
+            <h1 className='font-bold text-xl tracking-tighter text-gray-700'>Performance</h1>
+            <div className='grid grid-cols-2 '>
+              {
+                unread.length >= 1 ?
+                  <>
+                    <div className='col-span-2 mt-2 flex'>
+                      <ul>
+                        {unread.map((mesaage) => {
+                          return (
+                            <li key={mesaage.id} className='text-sm mt-3'><b>{mesaage.sender}<span className='text-xs text-gray-500'>({mesaage.sender_role})</span></b> - {mesaage.message}</li>
+                          )
+                        })}
+                      </ul>
+                    </div>
+                  </> :
+                  <>
+                    <div className='col-span-2 mt-20 flex justify-center items-center'>
+                      <p>Upcoming feature</p>
+                    </div>
+                  </>
+              }
+            </div>
+          </div>
         </div>
+
       </div>
 
       {/* <div className='container_own'>
